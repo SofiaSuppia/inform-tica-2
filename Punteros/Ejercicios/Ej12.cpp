2ef681a7596362ffaabf1c0b32eeb3ac4b8bcf2c
SofiaSuppia <111128577+SofiaSuppia@users.noreply.github.com>
2023-12-11 17:04:03 -0300

    14 es uno propio

---
+++ Punteros/Ejercicios/Ej12.cpp
@@ -0,0 +1,16 @@
+/* Desarrolle un programa que declare e inicialice un vector de 5 valores aleatorios. 
+Luego, crear 3 funciones que permita hallar el valor maximo, minimo y el sumatoria de 
+ellos. Finalmente mostrar los resultados. 
+Utilice punteros para realizar el ejercicio.*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
